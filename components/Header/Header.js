92caca8fb67aa579da41c133d45957788c33ff20
Jack <jackacbuchanan@gmail.com>
2021-03-16 21:26:56 +0000

    Second Commit

---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            Test
+        </div>
+    )
+}
+
+export default Header
